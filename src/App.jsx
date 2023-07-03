52c6410af89c7ebae9cd8f53f4a089db11c7196e
evans2919 <evansalcantara29@outlook.com>
2023-07-03 21:31:51 +0200

    Add archivo de indicaciones FrontEnd Mentor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline sm:text-center">Hello world!</h1>;
-}
+    return (
+        <h1 className="text-3xl font-bold underline sm:text-center">
+            Hello world!
+        </h1>
+    );
+};
 
 export default App;
