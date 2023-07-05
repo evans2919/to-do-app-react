1322b0119089894e056fc7c1f2195e9378b18ce7
evans2919 <evansalcantara29@outlook.com>
2023-07-05 21:56:39 +0200

    Añadiendo lógica de visuzalización y CRUD a ToDoList

--- src/components/ToDoBody.jsx
+++ src/components/ToDoBody.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/prop-types */
 import ToDoList from "./ToDoList";
-const ToDoBody = ({ toDo }) => {
+const ToDoBody = ({ toDo, deleteToDo, updateToDo}) => {
     return (
         <main className="container mx-auto mt-4 px-4">
             <div className="overflow-hidden rounded-md [&>article]:px-5 [&>article]:py-3 ">
                 {toDo.map((toDo) => (
-                    <ToDoList key={toDo.id} toDo={toDo} />
+                    <ToDoList key={toDo.id} toDo={toDo} deleteToDo={deleteToDo} updateToDo={updateToDo} />
                 ))}
 
-                <article className="flex justify-between  bg-white text-sm font-medium text-gray-400">
-                    <span>{toDo.length + " " + "items left"}</span>
-                    <button>Clear completed</button>
-                </article>
+                {toDo.length >= 1 && (
+                    <article className="flex justify-between  bg-white text-sm font-medium text-gray-400">
+                        <span>{toDo.length + " " + "items left"}</span>
+                        <button>Clear completed</button>
+                    </article>
+                )}
             </div>
         </main>
     );
