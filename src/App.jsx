b0ec3648ccbe41c5fd885b02c932d65daff6821f
evans2919 <evansalcantara29@outlook.com>
2023-07-05 21:55:26 +0200

    Creación CRUD v1

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,41 @@ import ToDoFilters from "./components/ToDoFilters";
 import ToDoFooter from "./components/ToDoFooter";
 import { useState } from "react";
 
-const initialStateToDo = [
-    {
-        id: 1,
-        title: "Recoger al perro",
-        state: true,
-    },
-    {
-        id: 2,
-        title: "Recoger al perro",
-        state: false,
-    },
-    {
-        id: 3,
-        title: "Recoger al perro",
-        state: true,
-    }
-
-]
+const initialStateToDo = [];
 
 const App = () => {
     const [toDo, setToDo] = useState(initialStateToDo);
 
+    const createToDo = (newToDo) => {
+        setToDo([...toDo, newToDo]);
+    };
 
+    const deleteToDo = (id) => {
+        const newArray = toDo.filter((toDo) => toDo.id !== id);
+        setToDo(newArray);
+    };
 
-
+    const updateToDo = (id) => {
+        const newArray = toDo.map((toDo) => {
+            if (toDo.id === id) {
+                toDo.state = !toDo.state;
+            }
+            return toDo;
+        });
+        setToDo(newArray);
+    };
 
     return (
         <>
             <div className="min-h-screen  bg-gray-100 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-top bg-no-repeat">
-                <ToDoHeader />
-                <ToDoBody toDo={toDo} />
-                <ToDoFilters />
-                <ToDoFooter />
+                <ToDoHeader createToDo={createToDo} />
+                <ToDoBody
+                    toDo={toDo}
+                    deleteToDo={deleteToDo}
+                    updateToDo={updateToDo}
+                />
+                <ToDoFilters toDo={toDo} />
+                <ToDoFooter toDo={toDo} />
             </div>
         </>
     );
