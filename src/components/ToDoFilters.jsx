e576a6c83807e5095658ba3908c07caed6d786ce
evans2919 <evansalcantara29@outlook.com>
2023-07-05 21:57:14 +0200

    Añadiendo lógica de visualización v1

--- src/components/ToDoFilters.jsx
+++ src/components/ToDoFilters.jsx
@@ -1,12 +1,19 @@
-const ToDoFilters = () => {
+/* eslint-disable react/prop-types */
+const ToDoFilters = ({toDo}) => {
     return (
-        <section className="container mx-auto mt-4 px-4">
-            <article className="flex justify-center gap-2 rounded-md bg-white px-5 py-3 text-sm font-semibold text-gray-400">
-                <button className="text-blue-600">All</button>
-                <button className="hover:text-blue-600">Active</button>
-                <button className="hover:text-blue-600">Completed</button>
-            </article>
-        </section>
+        <>
+            {toDo.length >= 1 && (
+                <section className="container mx-auto mt-4 px-4">
+                    <article className="flex justify-center gap-2 rounded-md bg-white px-5 py-3 text-sm font-semibold text-gray-400">
+                        <button className="text-blue-600">All</button>
+                        <button className="hover:text-blue-600">Active</button>
+                        <button className="hover:text-blue-600">
+                            Completed
+                        </button>
+                    </article>
+                </section>
+            )}
+        </>
     );
 }
 export default ToDoFilters;
--- src/components/ToDoFooter.jsx
+++ src/components/ToDoFooter.jsx
@@ -1,11 +1,18 @@
-const ToDoFooter = () => {
+/* eslint-disable react/prop-types */
+const ToDoFooter = ({ toDo }) => {
     return (
-        <section className="container mx-auto mt-6 px-4">
-            <article className="px-5 py-3 font-semibold text-gray-400 ">
-                <p className="text-center">Drag and drop to reorder list</p>
-            </article>
-        </section>
+        <>
+            {toDo.length >= 1 && (
+                <section className="container mx-auto mt-6 px-4">
+                    <article className="px-5 py-3 font-semibold text-gray-400 ">
+                        <p className="text-center">
+                            Drag and drop to reorder list
+                        </p>
+                    </article>
+                </section>
+            )}
+        </>
     );
-}
+};
 
-export default ToDoFooter
+export default ToDoFooter;
