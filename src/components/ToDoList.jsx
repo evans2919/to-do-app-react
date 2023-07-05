36c3262ac999633112a8fdb83384c1c42afe308b
evans2919 <evansalcantara29@outlook.com>
2023-07-05 21:59:24 +0200

    Añadiendo lógica CRUD a componente TodoList

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,16 +1,36 @@
 /* eslint-disable react/prop-types */
 import CrossIcon from "./icons/CrossIcon";
-const ToDoList = ({ toDo }) => {
+import CheckIcon from "./icons/CheckIcon";
+const ToDoList = ({ toDo, deleteToDo, updateToDo }) => {
     const { id, title, state } = toDo;
     return (
         <article className="flex justify-between border-b-2 border-b-gray-200 bg-white">
             <div className="flex items-center gap-2">
-                <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-                <p className="text-sm font-semibold text-gray-600">
+                {state ? (
+                    <button
+                        className="inline-block h-5 w-5 rounded-full"
+                        onClick={() => updateToDo(id)}
+                    >
+                        <CheckIcon />
+                    </button>
+                ) : (
+                    <button
+                        className="inline-block h-5 w-5 rounded-full border-2"
+                        onClick={() => updateToDo(id)}
+                    ></button>
+                )}
+
+                <p
+                    className={
+                        state
+                            ? "text-sm font-semibold text-gray-400 line-through"
+                            : "text-sm font-semibold text-gray-600"
+                    }
+                >
                     {title}
                 </p>
             </div>
-            <button>
+            <button onClick={() => deleteToDo(id)}>
                 <CrossIcon />
             </button>
         </article>
