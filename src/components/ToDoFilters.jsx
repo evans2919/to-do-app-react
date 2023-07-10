215260c7d83493f71ee989f904ac52af2bd1f6a0
evans2919 <evansalcantara29@outlook.com>
2023-07-10 19:47:49 +0200

    Refactorización de código de componente filter

--- src/components/ToDoFilters.jsx
+++ src/components/ToDoFilters.jsx
@@ -1,44 +1,39 @@
 /* eslint-disable react/prop-types */
+const filterStyles = {
+    active: "text-blue-600 hover:text-gray-400",
+    inactive: "text-gray-400 hover:text-blue-600",
+};
+
 const ToDoFilters = ({ toDo, changeFilter, filterToDo }) => {
+    const filterButton = (filter) => {
+        const isActive = filter === filterToDo;
+        const buttonStyle = isActive
+            ? filterStyles.active
+            : filterStyles.inactive;
+
+        return (
+            <button
+                className={buttonStyle}
+                onClick={() => changeFilter(filter)}
+            >
+                {filter.charAt(0).toUpperCase() + filter.slice(1)}
+            </button>
+        );
+    };
+
     return (
         <>
             {toDo.length >= 1 && (
                 <section className="container mx-auto mt-4 px-4">
                     <article className="flex justify-center gap-2 rounded-md bg-white px-5 py-3 text-lg font-semibold text-gray-400">
-                        <button
-                            className={
-                                filterToDo === "all"
-                                    ? "text-blue-600 hover:text-gray-400"
-                                    : "text-gray-400 hover:text-blue-600"
-                            }
-                            onClick={() => changeFilter("all")}
-                        >
-                            All
-                        </button>
-                        <button
-                            className={
-                                filterToDo === "active"
-                                    ? "text-blue-600 hover:text-gray-400"
-                                    : "text-gray-400 hover:text-blue-600"
-                            }
-                            onClick={() => changeFilter("active")}
-                        >
-                            Active
-                        </button>
-                        <button
-                            className={
-                                filterToDo === "completed"
-                                    ? "text-blue-600 hover:text-gray-400"
-                                    : "text-gray-400 hover:text-blue-600"
-                            }
-                            onClick={() => changeFilter("completed")}
-                        >
-                            Completed
-                        </button>
+                        {filterButton("all")}
+                        {filterButton("active")}
+                        {filterButton("completed")}
                     </article>
                 </section>
             )}
         </>
     );
 };
+
 export default ToDoFilters;
