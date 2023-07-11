f80a5b7a5460b9f7cd781cf2f42c09b1b1d968f3
evans2919 <evansalcantara29@outlook.com>
2023-07-11 21:10:14 +0200

    Add icon sol. add useState darkMode, add useEffect para theme localStorage

--- src/components/ToDoHeader.jsx
+++ src/components/ToDoHeader.jsx
@@ -1,15 +1,33 @@
 /* eslint-disable react/prop-types */
-import MoonIcon from "./icons/IconMoon";
+import { useEffect, useState } from "react";
+import IconMoon from "./icons/IconMoon";
+import IconSun from "./icons/IconSun";
 import ToDoForm from "./ToDoForm";
+
+const initialStateDarkMode =
+    document.documentElement.className.includes("dark");
+
 const ToDoHeader = ({ createToDo }) => {
+    const [darkMode, setDarkMode] = useState(initialStateDarkMode);
+
+    useEffect(() => {
+        if (darkMode) {
+             document.documentElement.classList.add("dark");
+             localStorage.theme = "dark";
+        } else {
+            document.documentElement.classList.remove("dark");
+           localStorage.theme = "light";
+        }
+    }, [darkMode]);
+
     return (
         <header className="container mx-auto px-4 pt-8">
             <div className="flex justify-between">
-                <h1 className="text-3xl font-semibold uppercase tracking-[0.3em] text-white">
+                <h1 className="text-3xl font-bold uppercase tracking-[0.3em] text-white">
                     Todo
                 </h1>
-                <button>
-                    <MoonIcon fill="#fff" />
+                <button onClick={() => setDarkMode(!darkMode)}>
+                    {darkMode ? <IconSun /> : <IconMoon fill="#fff" />}
                 </button>
             </div>
 
