5f9bd7de6ee53231eba952aed57dbb63fb6e108b
evans2919 <evansalcantara29@outlook.com>
2023-07-13 00:50:53 +0200

    Add custom bg-image desktop-mobile dark-light

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                "light-img": "url('/images/bg-desktop-light.jpg')",
+                "dark-img": "url('/images/bg-desktop-dark.jpg')",
+                "mobile-light-img": "url('/images/bg-mobile-light.jpg')",
+                "mobile-dark-img": "url('/images/bg-mobile-dark.jpg')",
+            },
+        },
     },
     plugins: [],
     darkMode: "class",
