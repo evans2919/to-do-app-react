8a9a2dbbe7f07e2a781c14b987f81b032ea35337
evans2919 <evansalcantara29@outlook.com>
2023-07-14 18:40:52 +0200

    Añadiendo librería DnD, método para mantener la ordenación y contexto DnD

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import ToDoHeader from "./components/ToDoHeader";
 import ToDoBody from "./components/ToDoBody";
 import ToDoFilters from "./components/ToDoFilters";
 import ToDoFooter from "./components/ToDoFooter";
+import { DragDropContext } from "@hello-pangea/dnd";
+
 import { useEffect, useState } from "react";
 
 const initialStateToDo = JSON.parse(localStorage.getItem("toDo")) || [];
@@ -53,19 +55,37 @@ const App = () => {
         }
     };
 
+    const handleDragEnd = (result) => {
+        const { source, destination } = result;
+        if (!destination) return;
+        if (
+            source.index === destination.index &&
+            source.droppableId === destination.droppableId
+        )
+            return;
+        const copyArray = [...toDo];
+        const [reorderedItem] = copyArray.splice(source.index, 1);
+        copyArray.splice(destination.index, 0, reorderedItem);
+        setToDo(copyArray);
+    };
+
     return (
         <div className="bg-custom min-h-screen bg-gray-100 bg-mobile-light-img bg-no-repeat transition-all duration-500 dark:bg-[#181824] dark:bg-mobile-dark-img lg:bg-light-img dark:lg:bg-dark-img">
             <main className="container mx-auto pt-10 lg:max-w-xl">
                 <ToDoHeader createToDo={createToDo} />
-                <ToDoBody
-                    toDo={filteredToDos()}
-                    deleteToDo={deleteToDo}
-                    updateToDo={updateToDo}
-                    deleteCompleted={deleteCompleted}
-                    toDoItemsLeft={toDoItemsLeft}
-                    changeFilter={changeFilter}
-                    filterToDo={filterToDo}
-                />
+
+                <DragDropContext onDragEnd={handleDragEnd}>
+                    <ToDoBody
+                        toDo={filteredToDos()}
+                        deleteToDo={deleteToDo}
+                        updateToDo={updateToDo}
+                        deleteCompleted={deleteCompleted}
+                        toDoItemsLeft={toDoItemsLeft}
+                        changeFilter={changeFilter}
+                        filterToDo={filterToDo}
+                    />
+                </DragDropContext>
+
                 <div className="lg:hidden">
                     <ToDoFilters
                         changeFilter={changeFilter}
