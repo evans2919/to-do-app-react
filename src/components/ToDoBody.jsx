0848a3ba65ab5b86d1b1d34aba3db3dc30b55396
evans2919 <evansalcantara29@outlook.com>
2023-07-14 18:42:19 +0200

    Add librería DnD y componentes DnD

--- src/components/ToDoBody.jsx
+++ src/components/ToDoBody.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import ToDoList from "./ToDoList";
 import ToDoFilters from "./ToDoFilters";
+import { Droppable, Draggable } from "@hello-pangea/dnd";
 const ToDoBody = ({
     toDo,
     deleteToDo,
@@ -13,14 +14,36 @@ const ToDoBody = ({
     return (
         <main className="container mx-auto mt-4 px-4 ">
             <div className="overflow-hidden rounded-md shadow-2xl [&>article]:px-5 [&>article]:py-3 ">
-                {toDo.map((toDo) => (
-                    <ToDoList
-                        key={toDo.id}
-                        toDo={toDo}
-                        deleteToDo={deleteToDo}
-                        updateToDo={updateToDo}
-                    />
-                ))}
+                <>
+                    <Droppable droppableId="todos">
+                        {(droppableProvider) => (
+                            <div
+                                ref={droppableProvider.innerRef}
+                                {...droppableProvider.droppableProps}
+                            >
+                                {toDo.map((toDo, index) => (
+                                    <Draggable
+                                        draggableId={`${toDo.id}`}
+                                        key={toDo.id}
+                                        index={index}
+                                    >
+                                        {(draggableProvider) => (
+                                            <ToDoList
+                                                ref={draggableProvider.innerRef}
+                                                {...draggableProvider.dragHandleProps}
+                                                {...draggableProvider.draggableProps}
+                                                toDo={toDo}
+                                                deleteToDo={deleteToDo}
+                                                updateToDo={updateToDo}
+                                            />
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {droppableProvider.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </>
 
                 <article className="flex justify-between  bg-white font-normal text-gray-400 dark:bg-[#25273c] dark:text-gray-500">
                     <span>{toDoItemsLeft} items left</span>
